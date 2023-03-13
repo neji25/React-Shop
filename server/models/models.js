41936e6d5690c3bba9bd1ab9ff54ce70c8541057
Vlad <myborvlad@gmail.com>
2023-03-13 23:55:11 +0300

    13.03.2023

--- server/models/models.js
+++ server/models/models.js
@@ -21,6 +21,7 @@ const Device = sequelize.define('device', {
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
     price: {type: DataTypes.INTEGER, allowNull: false},
     rating: {type: DataTypes.INTEGER, defaultValue: 0},
+    img: {type: DataTypes.STRING, allowNull: false}
 })
 
 const Type = sequelize.define('type', {
@@ -71,7 +72,7 @@ Rating.belongsTo(Device)
 Device.hasOne(BasketDevice)
 BasketDevice.belongsTo(Device)
 
-Device.hasMany(DeviceInfo)
+Device.hasMany(DeviceInfo, {as: 'info'})
 DeviceInfo.belongsTo(Device)
 
 Type.belongsToMany(Brand, {through: TypeBrand})
