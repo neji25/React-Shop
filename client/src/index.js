d7d259597056b63e070355078ec6f01ae46e5d87
Vlad <myborvlad@gmail.com>
2023-06-07 16:18:55 +0300

    Added interceptor

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import DeviceStore from "./store/DeviceStore";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const Context = createContext(null)
+console.log(process.env.REACT_APP_API_URL)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
